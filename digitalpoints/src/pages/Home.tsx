306cd1cc20965cd834f11a217b161c6a259da930
nelsonnovath13-nel <nelsonnovath13@gmail.com>
2026-08-18 15:40:15 +0300

    Place services intro after hero

--- digitalpoints/src/pages/Home.tsx
+++ digitalpoints/src/pages/Home.tsx
@@ -9,8 +9,8 @@ export default function Home() {
   return (
     <>
       <Hero />
-      <ImageMosaic />
       <Services />
+      <ImageMosaic />
       <VideoShowcase />
       <Partners />
       <FinalCTA />
