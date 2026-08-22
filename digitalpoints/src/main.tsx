def27bf2c3f365da66a8a704922a30794c64b213
nelsonnovath13-nel <nelsonnovath13@gmail.com>
2026-08-22 12:16:30 +0300

    feat: enable refined scroll transition

--- digitalpoints/src/main.tsx
+++ digitalpoints/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./mobile-hero-fix.css";
+import "./scroll-transition.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
