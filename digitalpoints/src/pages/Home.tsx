67df4c0ec388c968978c7acbfb230dd122955adf
nelsonnovath13-nel <nelsonnovath13@gmail.com>
2026-08-22 22:34:41 +0300

    Connect sticky services to homepage

--- digitalpoints/src/pages/Home.tsx
+++ digitalpoints/src/pages/Home.tsx
@@ -2,6 +2,7 @@ import Hero from "../components/Hero";
 import ImageMosaic from "../components/ImageMosaic";
 import Services from "../components/Services";
 import SectionRevealBidirectional from "../components/SectionRevealBidirectional";
+import StickyServices from "../components/StickyServices";
 import VideoShowcase from "../components/VideoShowcase";
 import Partners from "../components/Partners";
 import FinalCTA from "../components/FinalCTA";
@@ -13,6 +14,7 @@ export default function Home() {
       <SectionRevealBidirectional>
         <Services />
       </SectionRevealBidirectional>
+      <StickyServices />
       <ImageMosaic />
       <VideoShowcase />
       <Partners />
