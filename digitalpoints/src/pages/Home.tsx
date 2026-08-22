65adb88075ffb632106189d5d9dda4d95811d773
nelsonnovath13-nel <nelsonnovath13@gmail.com>
2026-08-22 13:59:07 +0300

    fix: remove services cards from homepage

--- digitalpoints/src/pages/Home.tsx
+++ digitalpoints/src/pages/Home.tsx
@@ -1,6 +1,5 @@
 import Hero from "../components/Hero";
 import ImageMosaic from "../components/ImageMosaic";
-import Services from "../components/Services";
 import VideoShowcase from "../components/VideoShowcase";
 import Partners from "../components/Partners";
 import FinalCTA from "../components/FinalCTA";
@@ -9,7 +8,6 @@ export default function Home() {
   return (
     <>
       <Hero />
-      <Services />
       <ImageMosaic />
       <VideoShowcase />
       <Partners />
